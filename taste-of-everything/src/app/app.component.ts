de2ce08ee7f7f36f768b96d52512c22245e7d62f
RAJA@9900 <raja9900ganesh@gmail.com>
2021-10-01 21:02:28 +0530

    commit

---
+++ taste-of-everything/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CustomerService } from './service/customer/customer.service';
+import { EmployeeService } from './service/employee/employee.service';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'taste-of-everything';
+
+  constructor(private customerService: CustomerService, private employeeService: EmployeeService, private router: Router) { }
+  public logout() {
+    this.customerService.isAuthenticated = false;
+    this.employeeService.isAuthenticated = false;
+    this.customerService.isCustomerLogin = false;
+    //sessionStorage.removeItem("customerName");
+    this.router.navigate(["/login"]);
+  }
+
+
+}
